7c821d840c6ff475bad53a8e98da0d91840eb4df
Fadoon T <fadoon.tar@gmail.com>
2024-03-22 13:25:23 +0100

    config

--- client-app/url_shortener/src/helpers/Constants.ts
+++ client-app/url_shortener/src/helpers/Constants.ts
@@ -1,2 +1,3 @@
 // Fetching the environment variables. We can use this server URL to make the API call
-export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:3002/api"
+// export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:3002/api";
+export const serverUrl = "https://url-shortener-clo3.onrender.com/api"
